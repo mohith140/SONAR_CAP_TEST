3cc242a5ac9f072506fcbda4fba2f0ad8dc45cfe
Mohith GILAKATHULA <gilakamo>
2025-09-04 06:13:08 +0000

    kl

--- srv/unsafe.js
+++ srv/unsafe.js
@@ -1,7 +1,18 @@
 const cds = require('@sap/cds');
-const m=require('../db/tables');
-module.exports = cds.service.impl(srv => {
-  srv.on('READ', 'Books', req => {
-    console.log(req)
+const { SELECT } = cds.ql;
+const m = require('../db/tables'); // unused, unless you need it
+
+module.exports = cds.service.impl(function (srv) {
+  // Example READ handler for Books
+  srv.on('READ', 'Bookstore', async req => {
+    console.log(req);
+
+    return this.tx(
+      { user: new cds.User.Privileged("") },
+      tx => tx.run(
+        SELECT.from('Bookstore') // match entity name in service2.cds
+          .where`Bookstore = ${req.data.messageToPass}`
+      )
+    );
   });
 });
