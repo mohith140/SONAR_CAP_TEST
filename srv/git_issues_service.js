77ee5b1f721d4f8c199832e316c1d3e8a81f3cc7
Mohith GILAKATHULA <gilakamo>
2025-09-08 12:14:42 +0000

    l

--- srv/git_issues_service.js
+++ srv/git_issues_service.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({ secret: process.env['SECRET'], cookie: { maxAge: 60000 } }));
 
 // ...
-
+ 
 app.post("/changeEmail", function(req, res) {
   const userId = req.session.id;
   const email = req.body["email"];
